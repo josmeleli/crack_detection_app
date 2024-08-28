8e9dea074ba43076cc4ff98c8823fc3c0c3b3c92
josmel <josmelvt@gmail.com>
2024-08-27 21:10:31 -0500

    detectar grietas api

--- lib/pages/detection_page.dart
+++ lib/pages/detection_page.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/appbar_widget.dart';
 import 'package:flutter_application_1/widgets/btn_widget.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
 
 class DetectionPage extends StatefulWidget {
   const DetectionPage({super.key});
@@ -14,6 +18,179 @@ class DetectionPage extends StatefulWidget {
 
 class DetectionPageState extends State<DetectionPage> {
   XFile? _image;
+
+Future<void> _sendImageToAPI(BuildContext context) async {
+  if (_image != null) {
+    try {
+      // Obtén el usuario actual de Firebase
+      final user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        final userId = user.uid;
+
+        // Prepara la imagen para el envío
+        var request = http.MultipartRequest(
+          'POST',
+          Uri.parse('http://10.0.2.2:8000/predict/')
+        );
+
+        request.files.add(await http.MultipartFile.fromPath('file', _image!.path));
+
+        var response = await request.send();
+
+        if (response.statusCode == 200) {
+          final responseData = await response.stream.bytesToString();
+          final decodedData = json.decode(responseData);
+
+          // Verifica que las claves existan y no sean nulas
+          if (decodedData.containsKey('max_crack_width') && decodedData['max_crack_width'] != null) {
+            double maxCrackWidth = decodedData['max_crack_width'];
+            String maxCrackWidthStr = maxCrackWidth.toStringAsFixed(2);
+
+            // Envía los resultados a Firestore
+            await _sendResultsToFirestore(userId, maxCrackWidthStr);
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Resultados enviados exitosamente')),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Respuesta de la API inválida')),
+            );
+          }
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Error al procesar la imagen')),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Usuario no autenticado')),
+        );
+      }
+    } catch (e) {
+      print("Error al enviar la imagen: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error al enviar la imagen')),
+      );
+    }
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Por favor selecciona una imagen')),
+    );
+  }
+}
+
+Future<void> _sendResultsToFirestore(String userId, String maxCrackWidth) async {
+  try {
+    // Crea una nueva referencia de documento para almacenar los resultados
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('results')
+        .add({
+      'max_crack_width': maxCrackWidth,
+      'timestamp': FieldValue.serverTimestamp(), // Añadir marca de tiempo
+    });
+  } catch (e) {
+    print("Error al enviar los resultados a Firestore: $e");
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Error al guardar los resultados en Firestore')),
+    );
+  }
+}
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final double containerWidth = MediaQuery.of(context).size.width * 0.8;
+
+    return Scaffold(
+      appBar: const CustomAppBar(
+        title: 'Detectar',
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Muestra la imagen seleccionada o un mensaje
+              Container(
+                margin: const EdgeInsets.only(bottom: 16.0),
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: Container(
+                        width: containerWidth,
+                        height: containerWidth,
+                        color: const Color(0xFFF4F4F4),
+                        child: _image == null
+                            ? const Center(child: Text('Imagen aquí'))
+                            : Image.file(
+                                File(_image!.path),
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                  ],
+                ),
+              ),
+              // Botón para enviar la imagen a la API y procesar resultados
+              Container(
+                width: containerWidth,
+                margin: const EdgeInsets.only(bottom: 8.0),
+                child: ButtonWidget(
+                  onClick: () => _sendImageToAPI(context),
+                  btnText: 'Detectar',
+                  icon: Icons.broken_image,
+                ),
+              ),
+              // Botón para abrir la cámara
+              Container(
+                width: containerWidth,
+                margin: const EdgeInsets.only(bottom: 8.0),
+                child: ButtonWidget(
+                  onClick: _openCamera,
+                  btnText: 'Abrir Cámara',
+                  icon: Icons.camera_alt,
+                ),
+              ),
+              // Botón para subir la imagen desde la galería
+              Container(
+                width: containerWidth,
+                margin: const EdgeInsets.only(bottom: 8.0),
+                child: ButtonWidget(
+                  onClick: _pickImage,
+                  btnText: 'Subir Imagen',
+                  icon: Icons.upload_file,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _openCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      final ImagePicker picker = ImagePicker();
+      final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+      if (image != null && mounted) {
+        setState(() {
+          _image = image;
+        });
+      }
+    } else if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Permiso de cámara denegado')),
+      );
+    }
+  }
 
   Future<void> _pickImage() async {
     final hasPermission = await _checkPermissions();
@@ -36,106 +213,11 @@ class DetectionPageState extends State<DetectionPage> {
 
   Future<bool> _checkPermissions() async {
     if (Platform.isAndroid) {
-      // En Android, revisa los permisos de fotos y almacenamiento
       return await Permission.photos.request().isGranted ||
           await Permission.storage.request().isGranted;
     } else if (Platform.isIOS) {
-      // En iOS, solo revisa el permiso de fotos
       return await Permission.photos.request().isGranted;
     }
     return false;
-  }
-
-  Future<void> _openCamera() async {
-    // Solicitar permisos de cámara
-    if (await Permission.camera.request().isGranted) {
-      final ImagePicker picker = ImagePicker();
-      final XFile? image = await picker.pickImage(source: ImageSource.camera);
-
-      if (image != null && mounted) {
-        setState(() {
-          _image = image;
-        });
-      }
-    } else if (mounted) {
-      // Manejar el caso en que los permisos no son concedidos
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Permiso de cámara denegado')),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double containerWidth = MediaQuery.of(context).size.width * 0.8;
-
-    return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'Detectar',
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 16.0),
-                child: Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius:
-                          BorderRadius.circular(16.0), // Bordes redondeados
-                      child: Container(
-                        width:
-                            containerWidth, // Hacer el contenedor más pequeño
-                        height: containerWidth, // Hacer el contenedor cuadrado
-                        color: const Color(0xFFF4F4F4), // Color ahuesado
-                        child: _image == null
-                            ? const Center(child: Text('Imagen aquí'))
-                            : Image.file(
-                                File(_image!.path),
-                                fit: BoxFit.cover,
-                              ),
-                      ),
-                    ),
-                    const SizedBox(height: 8.0),
-                  ],
-                ),
-              ),
-              Container(
-                width: containerWidth,
-                margin: const EdgeInsets.only(bottom: 8.0),
-                child: ButtonWidget(
-                  onClick: () {
-                    // Lógica para detectar
-                  },
-                  btnText: 'Detectar',
-                  icon: Icons.broken_image,
-                ),
-              ),
-              Container(
-                width: containerWidth,
-                margin: const EdgeInsets.only(bottom: 8.0),
-                child: ButtonWidget(
-                  onClick: _openCamera,
-                  btnText: 'Abrir Cámara',
-                  icon: Icons.camera_alt,
-                ),
-              ),
-              Container(
-                width: containerWidth,
-                margin: const EdgeInsets.only(bottom: 8.0),
-                child: ButtonWidget(
-                  onClick: _pickImage,
-                  btnText: 'Subir Imagen',
-                  icon: Icons.upload_file,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
